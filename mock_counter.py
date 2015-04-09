f339fd670e253576305812c0fbf91e214f0a18a6
ahaith <pi@raspberrypi.(none)>
2015-04-09 16:45:45 +0000

    Successfully driving video from mock data

---
+++ mock_counter.py
@@ -0,0 +1,48 @@
+#video=about 30 frames per second
+import time
+import random
+import zope.event
+
+
+
+def mock():
+	print "STARTING MOCK"
+	minincrement=-2
+	maxincrement=3
+	increment=0
+	maxcount=45000
+	dist=0 #in metres
+	count=0
+	timeinterval=1/120
+	while count<maxcount:  
+  	  #waits 1/120 seconds(time between each check)
+  	  time.sleep(timeinterval)
+  	  #bias the incrementdelta when limit is approached
+  	  if increment>10:
+   	     maxincrement=1
+  	  if (maxincrement<3 and increment<10):
+ 	       maxincrement=3
+ 	  if increment<3:
+ 	       minincrement=-1
+ 	  if (minincrement>-2 and increment>3):
+ 	       minincrement=-2
+  	  incrementdelta=random.randrange(minincrement,maxincrement)
+  	  #sets new increment to last increment + incrementdelta
+  	  newincrement=increment+incrementdelta
+   	  #checks if newincrement is bigger than 12 or smaller than 0
+   	  while (newincrement>12 or newincrement<0):     
+   	     if newincrement>12:     
+     	       newincrement=12   
+    	     else:   
+     	       newincrement=0    
+    	  #print("random increment was outside acceptable range, set newincrement to:", newincrement)  #alerts us that new increment is outside acceptable range
+ 	  #set increment to newincrement
+   	  increment=newincrement
+  	  #changes count by increment
+  	  count=count+increment       
+   	  #print(count, "increment:", increment)    #prints count
+  	  #time.sleep(0.25)    #gives us time to read the value of count
+
+	  #   create event with count
+ 	  print "SENDING COUNT EVENT", count
+	  zope.event.notify(count)
--- video.py
+++ video.py
@@ -1,18 +1,45 @@
 from pyomxplayer import OMXPlayer
 import time
 import zope.event
+from mock_counter import mock
+from threading import Thread
 
-omx = OMXPlayer('smaller.mp4', None, start_playback=True)
+# Keep track of running total count since last frame increment
+# TODO - the method can't see runningtotal. Put all this in a class.
 
-time.sleep(0.5)
 
-#paused
-omx.toggle_pause()
+#method to decide whether to increment frame
+def shouldAdvanceFrame(extracount):
+    shouldAdvanceFrame.runningtotal+=extracount
+    if shouldAdvanceFrame.runningtotal>50:
+        shouldAdvanceFrame.runningtotal-=50
+        return True
+    else:
+        return False
+shouldAdvanceFrame.runningtotal=0
 
-count = 0
-while(count<100):
-    omx.step()
-    time.sleep(0.1)
-    count+=1
+# Initialise video
+omx = OMXPlayer('smaller.mp4', None, start_playback=False)
 
+# Create event handler and subscribe to event
+def handleEvent(count):
+    print "got an event!"
+    if shouldAdvanceFrame(count):
+    	omx.step()
+	#print "step!"
+
+#subscribe to events
+zope.event.subscribers.append(handleEvent)
+
+print "subscribed"
+
+time.sleep(1)
+
+#Start up the mock
+thread = Thread(target=mock)
+thread.start()
+
+
+#kill the video after too long
+time.sleep(100)
 omx.stop()
