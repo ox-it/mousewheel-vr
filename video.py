df7818e85c497a3e6f703449e8a5166137064814
ahaith <pi@raspberrypi.(none)>
2015-04-10 10:25:57 +0000

    Send increment, not total count

--- video.py
+++ video.py
@@ -16,11 +16,8 @@ def shouldAdvanceFrame(extracount):
     else:
         return False
 
-
 shouldAdvanceFrame.runningtotal=0
 
-# Initialise video
-omx = OMXPlayer('smaller.mp4', '--no-osd', start_playback=False)
 
 # Create event handler and subscribe to event
 def handleEvent(count):
@@ -28,20 +25,23 @@ def handleEvent(count):
     if count < 0:
 	# We've reached the end of the input
 	print "Finished"
-	omx.toggle_pause()
-	omx.stop()
+	#omx.toggle_pause()
+	#omx.stop()
     
     elif shouldAdvanceFrame(count):
-    	omx.step()
-	BLAH = 1
-	
+    	#omx.step()
+
+
+# Initialise video
+#omx = OMXPlayer('smaller.mp4', '--no-osd', start_playback=False)
+
+# Get the video started, then wait a short while before beginning the mock thread
+#omx.step()
+sleep(1)	
 
 #subscribe to events
 zope.event.subscribers.append(handleEvent)
-
 print "subscribed"
-
-time.sleep(1)
 
 #Start up the mock
 thread = Thread(target=mock)
